a89f6f3e9d493267bf928ffb42f9dc21bf174bb0
Ponragavan M <mpragavan0209@gmail.com>
2024-10-02 21:17:35 +0530

    NODE_ENV

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -10,7 +10,9 @@ exports.setTokenCookie = (res, token, userId) => {
   res.cookie('token', token, {
     path: "/",
     expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 1 day
-    sameSite: "lax",
-    httpOnly: true
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    httpOnly: true,
+    domain: process.env.NODE_ENV === 'production' ? '.onrender.com' : 'localhost',
+    secure: process.env.NODE_ENV === 'production',
   });
 };
