a360eda07fed4cb0ba9a05a3c6bf4b72011d4189
Ponragavan M <mpragavan0209@gmail.com>
2024-10-02 16:19:35 +0530

    cookies

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -10,7 +10,6 @@ exports.setTokenCookie = (res, token, userId) => {
   res.cookie('token', token, {
     path: "/",
     expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 1 day
-    httpOnly: true,
     sameSite: "lax",
     secure: true
   });
